936c5d50b8245f99971dbfa299ca299bc1d7f660
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2023-01-31 09:50:06 -0500

    Initial commit.

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,tsx}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
